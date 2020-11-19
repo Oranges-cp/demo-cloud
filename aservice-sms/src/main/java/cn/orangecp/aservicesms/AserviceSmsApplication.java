cc4748ecc0486a2c357f45626ab899199ec71763
chenpan <chenpan@xinchao.com>
2020-11-19 16:54:46 +0800

    add first module

---
+++ aservice-sms/src/main/java/cn/orangecp/aservicesms/AserviceSmsApplication.java
@@ -0,0 +1,13 @@
+package cn.orangecp.aservicesms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AserviceSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AserviceSmsApplication.class, args);
+	}
+
+}
---
+++ aservice-sms/src/test/java/cn/orangecp/aservicesms/AserviceSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.orangecp.aservicesms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AserviceSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
